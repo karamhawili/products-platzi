d98609dba16f3cbd8968fddd11185b9d3942e350
Karam Abou Hawili <karam.hawili@gmail.com>
2026-05-25 17:52:34 +0300

    Fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getProducts, type Product } from "./api/platzi";
 import "./App.css";
 import { useFetch } from "./hooks/useFetch";
-import ProductCard from "./components/product-card/ProductCard";
 
 function App() {
   const { loading, error, data } = useFetch(getProducts);
   const [openModal, setOpenModal] = useState(false);
   const [activeItem, setActiveItem] = useState<Product | null>(null);
+  const [orderedData, setOrderedData] = useState<Product[] | null>(null);
+
+  const dragItem = useRef<string | null>(null);
+  const dragOver = useRef<string | null>(null);
 
   useEffect(() => {
-    localStorage.setItem("products", JSON.stringify(data));
+    if (!data) return;
+
+    const localData = localStorage.getItem("products");
+
+    if (!localData) {
+      setOrderedData(data);
+      return;
+    }
+
+    setOrderedData(JSON.parse(localData));
   }, [data]);
+
+  useEffect(() => {
+    if (!orderedData) return;
+
+    localStorage.setItem("products", JSON.stringify(orderedData));
+  }, [orderedData]);
 
   const onCardClick = (item: Product) => {
     setOpenModal(true);
@@ -21,6 +39,32 @@ function App() {
   const handleCloseClick = () => {
     setOpenModal(false);
     setActiveItem(null);
+  };
+
+  const handleDragStart = (id: string) => {
+    dragItem.current = id;
+  };
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    dragOver.current = (e.target as HTMLDivElement).id;
+  };
+
+  const handleDragEnd = () => {
+    const dataArr = [...(orderedData || [])];
+    const draggedItemIndex = dataArr?.findIndex(
+      (item) => `${item.id}` === `${dragItem.current}`,
+    );
+    const dragOverItemIndex = dataArr?.findIndex(
+      (item) => `${item.id}` === `${dragOver.current}`,
+    );
+
+    const ordered = dataArr?.map((val, i) => {
+      if (i === draggedItemIndex) return dataArr[dragOverItemIndex];
+      if (i === dragOverItemIndex) return dataArr[draggedItemIndex];
+      return val;
+    });
+
+    setOrderedData(ordered);
   };
 
   return (
@@ -33,10 +77,28 @@ function App() {
           </div>
         )}
         {error && <div className="error-container">{error}</div>}
-        {data && (
+        {orderedData && (
           <div className="product-grid">
-            {data.map((item) => (
-              <ProductCard item={item} onClick={() => onCardClick(item)} />
+            {orderedData.map((item) => (
+              <div
+                className="product-card"
+                draggable
+                onDragStart={() => handleDragStart(item.id)}
+                onDragEnter={handleDragEnter}
+                onDragEnd={handleDragEnd}
+                onClick={() => onCardClick(item)}
+                key={item.id}
+                id={item.id}
+              >
+                <div className="interactive" id={item.id}></div>
+                <div className="image">
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div className="meta">
+                  <div className="title">{item.title}</div>
+                  <div className="price">${item.price}</div>
+                </div>
+              </div>
             ))}
           </div>
         )}
